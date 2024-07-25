df62c369f4954e734f265b5ac829a311a98638ae
JoseeGarciaa <113802219+JoseeGarciaa@users.noreply.github.com>
2024-07-24 22:49:23 -0500

    Update consulta_mysql.py

--- consulta_mysql.py
+++ consulta_mysql.py
@@ -1,13 +1,22 @@
 import pymysql
 import pandas as pd
+import os
 
 def obtener_datos():
+    # Obtener las variables de entorno
+    host = os.getenv('DB_HOST')
+    user = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWORD')
+    db = os.getenv('DB_NAME')
+    port = int(os.getenv('DB_PORT', 3306))  # 3306 es el puerto por defecto de MySQL
+
     # Conexión a la base de datos
     conexion = pymysql.connect(
-        host='localhost',
-        user='tu_usuario',
-        password='tu_contraseña',
-        db='tu_base_de_datos'
+        host=host,
+        user=user,
+        password=password,
+        db=db,
+        port=port
     )
 
     # Realizar una consulta
