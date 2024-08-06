b61613dc6897c0ab94a88f2dae184bd7365bd39f
JoseeGarciaa <113802219+JoseeGarciaa@users.noreply.github.com>
2024-08-05 19:53:45 -0500

    Update generar_grafica.py

--- generar_grafica.py
+++ generar_grafica.py
@@ -9,23 +9,18 @@ def generar_grafica():
     print(df.columns)
 
     # Crear una gráfica
-    plt.figure(figsize=(14, 8))  # Aumentar el tamaño de la figura
+    plt.figure(figsize=(12, 8))  # Aumentar el tamaño
     plt.bar(df['Materia'], df['Cantidad'])
     plt.title('Cantidad de Registros por Materia')
     plt.xlabel('Materia')
     plt.ylabel('Cantidad')
-    
-    # Rotar las etiquetas y ajustar espaciado
-    plt.xticks(rotation=60, ha='right', fontsize=10, padding=5)  # Rotar a 60 grados y alinear a la derecha
+    plt.xticks(rotation=60, ha='right', fontsize=10)  # Rotar a 60 grados y alinear a la derecha
     plt.grid(True)
     
-    # Ajustar el espaciado entre los elementos de la gráfica
-    plt.subplots_adjust(bottom=0.25)  # Aumentar el espaciado en la parte inferior
-
     plt.tight_layout()  # Ajustar automáticamente el layout
     
     # Guardar la gráfica como un archivo
     plt.savefig('grafica.png')
 
 if __name__ == "__main__":
-    generar_grafica() 
+        generar_grafica() 
